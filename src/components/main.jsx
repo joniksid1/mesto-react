39ce47e7b6ab0f7429441be2d27864b14c0d25c9
Mikhail Kuznetsov <jonik.sid1@gmail.com>
2023-10-16 22:54:04 +0300

    feat: auto test fix 1

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import api from '../utils/api';
+import api from '../utils/Api';
 import Card from './card';
 
 function Main({ onEditProfile, onAddPlace, onEditAvatar, onCardClick }) {
